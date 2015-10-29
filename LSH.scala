1d5b6da1e35b4861881b762302fbf046e00d758f
parallelgithub <d97033@csie.ntu.edu.tw>
2015-10-29 19:02:52 +0800

    minus hash value

--- LSH.scala
+++ LSH.scala
@@ -1,10 +1,39 @@
 import scala.io.Source
+import scala.util.Random
 
 object LSH {
 	def main(args: Array[String]){
-		val fileLines = Source.fromFile("../dataset/sift/siftsmall/siftsmall_base.input").getLines.toList.map{_.split(" ").toList}
+		//val baseVector = Source.fromFile("../dataset/sift/siftsmall/test_base").getLines.toList.map{_.split(" ").toIndexedSeq}
+		val queryVectors = Source.fromFile("../dataset/sift/siftsmall/query.input").getLines.toList.map{_.split(" ").toIndexedSeq}
+		val learnVectors = Source.fromFile("../dataset/sift/siftsmall/learn.input").getLines.toList.map{_.split(" ").map{_.toDouble}.toIndexedSeq}
 
-		//fileLines: 10000 * 128 List
+		val dimention = learnVectors(1).length
+
+		//learnVectors: 25000 * 128 List
+		/*
+		for (query <- queryVectors){
+			if(learnVectors.contains(query))
+					println("Query #"+queryVectors.indexOf(query)+" is in learnVector #"+learnVectors.indexOf(query))
+		}
+		*/
+		
+		val parameterW = 4.0
+		var a = 0
+		for (vector <- learnVectors){
+
+			if(a<20){
+			val vectorA = IndexedSeq.fill(dimention)(Random.nextGaussian())
+			
+			val parameterB = Random.nextDouble() * parameterW
+
+			val dot = (vector zip vectorA).map{case (a,b) => a * b}.foldLeft(0.0)(_+_)
+			val hashValue = ((dot + parameterB)/parameterW).toInt
+			println(hashValue)
+			
+
+			a += 1
+			}
+		}
 
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # p-stable
+
+(?)檢索技術劃分: 分層法(較慢) vs 哈希碼法
+ 
+Algorithm:
+  (av + b)/w
+
+parameters:
+  a = 
+  w = 4.0
+	b is a real number chosen uniformly from [0,w] i.e. [0,4]
+	k 
+	L
+
