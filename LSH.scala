d53b49968ef66a58bfa89c0797f44537e12f0a17
parallelgithub <d97033@csie.ntu.edu.tw>
2015-10-30 14:39:58 +0800

    erfc, parameter L, function p

--- LSH.scala
+++ LSH.scala
@@ -1,7 +1,44 @@
 import scala.io.Source
 import scala.util.Random
+import scala.math
+
 
 object LSH {
+
+
+	val parameterW = 4.0
+
+	//from http://picomath.org/scala/Erf.scala.html
+	def ERFC(x: Double): Double =  {
+		val a1: Double =  0.254829592;
+		val a2: Double = -0.284496736;
+		val a3: Double =  1.421413741;
+		val a4: Double = -1.453152027;
+		val a5: Double =  1.061405429;
+		val p: Double  =  0.3275911;
+		// Save the sign of x
+		val sign = if (x < 0) -1 else 1
+		val absx =  math.abs(x)
+
+		// A&S formula 7.1.26, rational approximation of error function
+		val t = 1.0/(1.0 + p*absx);
+		val y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*math.exp(-x*x);
+		sign*y
+	}
+
+	//from E2LSH code in SelfTuring.cpp
+	def computeFunctionP(w: Double, c: Double):Double = {
+		  val x = w / c;
+			val M_SQRT2 = math.sqrt(2.0)
+			val M_2_SQRTPI = 2.0 / math.sqrt(math.Pi)
+			1 - ERFC(x / M_SQRT2) - M_2_SQRTPI / M_SQRT2 / x * (1 - math.exp(-(x*x) / 2));
+	}
+
+	def computeL(dd: Int, successProbability: Double):Int = {
+		  math.ceil(math.log(1 - successProbability) / math.log(1 - math.pow(computeFunctionP(parameterW, 1.0), dd))).toInt;
+	}
+
+
 	def main(args: Array[String]){
 		//val baseVector = Source.fromFile("../dataset/sift/siftsmall/test_base").getLines.toList.map{_.split(" ").toIndexedSeq}
 		val queryVectors = Source.fromFile("../dataset/sift/siftsmall/query.input").getLines.toList.map{_.split(" ").toIndexedSeq}
@@ -17,20 +54,18 @@ object LSH {
 		}
 		*/
 		
-		val parameterW = 4.0
 		var a = 0
 		for (vector <- learnVectors){
 
-			if(a<20){
 			val vectorA = IndexedSeq.fill(dimention)(Random.nextGaussian())
 			
 			val parameterB = Random.nextDouble() * parameterW
 
 			val dot = (vector zip vectorA).map{case (a,b) => a * b}.foldLeft(0.0)(_+_)
-			val hashValue = ((dot + parameterB)/parameterW).toInt
-			println(hashValue)
-			
-
+			val hashValue = math.floor((dot + parameterB)/parameterW).toInt
+			if(a<20){
+			//println("a = " + vectorA)
+			//println(hashValue)
 			a += 1
 			}
 		}
